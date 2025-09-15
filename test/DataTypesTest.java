36aacdd589ea8ac7ab95d8dfdee21b214b43e194
Kaifeng <kaifengp@gmail.com>
2025-09-15 13:46:40 -0400

    Task 1

--- test/DataTypesTest.java
+++ test/DataTypesTest.java
@@ -4,9 +4,9 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class DataTypesTest {
 
     @Test
-    /**
-     * Test that DataTypes.sum returns the correct value for
-     * the sum from 1 to 1 million.
+    /*
+      Test that DataTypes.sum returns the correct value for
+      the sum from 1 to 1 million.
      */
     public void largeSumTest() {
 
